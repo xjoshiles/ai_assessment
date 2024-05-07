e75493e9decc1ad5f90662cfae69d3219550e3e8
Josh Iles <JI91@canterbury.ac.uk>
2024-05-07 14:18:11 +0100

    Data reading, reduction, and cleaning

--- model_train.py
+++ model_train.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
+# Data preprocessing
 dataset = pd.read_csv('Dataset of Diabetes.csv')
+dataset = dataset.drop(['ID', 'No_Pation'], axis=1)
+dataset['Gender'] = dataset['Gender'].map(lambda g: g.upper())
+dataset['CLASS']  = dataset['CLASS'].map(lambda c: c.strip())
 

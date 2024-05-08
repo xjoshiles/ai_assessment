f9b3140e9d1f92712a10265c583c019736758875
Josh Iles <JI91@canterbury.ac.uk>
2024-05-08 18:12:42 +0100

    Split into training and test data with stratification

--- model_train.py
+++ model_train.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # Data preprocessing:
 dataset = pd.read_csv('Dataset of Diabetes.csv')
@@ -17,4 +18,8 @@ X['Gender'] = pd.Categorical(dataset['Gender']).codes
 
 # One-Hot encode output labels:
 Y = pd.get_dummies(Y)
+
+# Splitting the dataset into training and test sets:
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.2, random_state=1, stratify=Y)
 

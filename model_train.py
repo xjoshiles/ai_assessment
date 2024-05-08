fad23d9df967123983be178e4df482a1124837ef
Josh Iles <JI91@canterbury.ac.uk>
2024-05-08 17:33:03 +0100

    Encoding categorical data and labels

--- model_train.py
+++ model_train.py
@@ -2,9 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Data preprocessing
+# Data preprocessing:
 dataset = pd.read_csv('Dataset of Diabetes.csv')
 dataset = dataset.drop(['ID', 'No_Pation'], axis=1)
 dataset['Gender'] = dataset['Gender'].map(lambda g: g.upper())
 dataset['CLASS']  = dataset['CLASS'].map(lambda c: c.strip())
+
+# Splitting our dataset into features (X) and labels (Y):
+X = dataset.drop(['CLASS'], axis=1)
+Y = dataset['CLASS']
+
+# Encoding categorical features:
+X['Gender'] = pd.Categorical(dataset['Gender']).codes
+
+# One-Hot encode output labels:
+Y = pd.get_dummies(Y)
 
